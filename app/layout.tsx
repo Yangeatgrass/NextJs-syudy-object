4a83b5698cb96d79a993c71a498842a8aa8f99c9
run一下试逝 <1327370026@qq.com>
2023-12-11 15:23:33 +0800

    init object

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+// 导入全局样式
+import '@/app/ui/global.css';
+// 导入全局字体
+import { inter } from '@/app/ui/fonts'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
