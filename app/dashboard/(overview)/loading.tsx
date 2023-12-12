95b4453acb21d30555a19cc39230cdd2f9a7829a
run一下试逝 <1327370026@qq.com>
2023-12-12 10:50:13 +0800

    1，新增仪表盘信息 2，新增流式媒体 3，使用装包器优化流媒体

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,9 @@
+/**
+ *  *  流媒体文件
+ */
+
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -90,7 +90,7 @@ export default function DashboardSkeleton() {
       />
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <CardSkeleton />
-        <CardSkeleton />
+        <CardSkeleton /> 
         <CardSkeleton />
         <CardSkeleton />
       </div>
